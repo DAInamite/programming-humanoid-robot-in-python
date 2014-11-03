41b565ddad723c551f14c037bc6f19aabd88ac91
Xu Yuan <yuan.xu@dai-labor.de>
2014-11-03 10:50:06 +0100

    Posture Recognition exercise

---
+++ joint_control/recognize_posture.py
@@ -0,0 +1,40 @@
+'''In this exercise you need to use the learned classifier to recognize current posture of robot
+
+* Tasks:
+    1. load learned classifier in `PostureRecognitionAgent.__init__`
+    2. recognize current posture in `PostureRecognitionAgent.recognize_posture`
+
+* Hints:
+    Let the robot execute different keyframes, and recognize these postures.
+
+'''
+
+
+from angle_interpolation import AngleInterpolationAgent
+from keyframes import hello
+
+
+class PostureRecognitionAgent(AngleInterpolationAgent):
+    def __init__(self, simspark_ip='localhost',
+                 simspark_port=3100,
+                 teamname='DAInamite',
+                 player_id=0,
+                 sync_mode=True):
+        super(PostureRecognitionAgent, self).__init__(simspark_ip, simspark_port, teamname, player_id, sync_mode)
+        self.posture = 'unknown'
+        self.posture_classifier = None  # LOAD YOUR CLASSIFIER
+
+    def think(self, perception):
+        self.posture = self.recognize_posture(perception)
+        return super(PostureRecognitionAgent, self).think(perception)
+
+    def recognize_posture(self, perception):
+        posture = 'unknown'
+        # YOUR CODE HERE
+
+        return posture
+
+if __name__ == '__main__':
+    agent = AngleInterpolationAgent()
+    agent.keyframes = hello()  # CHANGE DIFFERENT KEYFRAMES
+    agent.run()
