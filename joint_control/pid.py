39af751332c1d85b0ef9e4bc448914993d3840d5
Maik <oneill244@gmx.de>
2015-11-03 15:55:28 +0100

    Bugfix: PID Controller

--- joint_control/pid.py
+++ joint_control/pid.py
@@ -35,9 +35,9 @@ class PIDController(object):
         self.e2 = np.zeros(size)
         # ADJUST PARAMETERS BELOW
         delay = 0
-        self.Kp = 50
-        self.Ki = 0.5
-        self.Kd = 1
+        self.Kp = 65
+        self.Ki = 0.7
+        self.Kd = -0.1
         self.y = deque(np.zeros(size), maxlen=delay + 1)
 
     def set_delay(self, delay):
@@ -65,7 +65,7 @@ class PIDController(object):
         self.e2, self.e1 = self.e1, e
         
         # Finish it
-        self.u = self.u + p + i + d
+        self.u = p + i + d
         return self.u
 
 
