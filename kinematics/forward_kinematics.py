447d6a17cfa7d911de3b625f2cb494dbc314f4ce
polocallens <polocallens@hotmail.fr>
2017-06-21 09:19:36 +0200

    ex3

--- kinematics/forward_kinematics.py
+++ kinematics/forward_kinematics.py
@@ -5,12 +5,11 @@
        The documentation from Aldebaran is here:
        http://doc.aldebaran.com/2-1/family/robots/bodyparts.html#effector-chain
     2. implement the calculation of local transformation for one joint in function
-       ForwardKinematicsAgent.local_trans. The necessary documentation are:
+       ForwardKinematicsAgent.ans. The necessary documentation are:
        http://doc.aldebaran.com/2-1/family/nao_h21/joints_h21.html
        http://doc.aldebaran.com/2-1/family/nao_h21/links_h21.html
     3. complete function ForwardKinematicsAgent.forward_kinematics, save the transforms of all body parts in torso
        coordinate into self.transforms of class ForwardKinematicsAgent
-
 * Hints:
     the local_trans has to consider different joint axes and link parameters for differnt joints
 '''
@@ -18,57 +17,115 @@
 # add PYTHONPATH
 import os
 import sys
+import math
 sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'joint_control'))
 
 from numpy.matlib import matrix, identity
+
 
 from angle_interpolation import AngleInterpolationAgent
 
 
 class ForwardKinematicsAgent(AngleInterpolationAgent):
-    def __init__(self, simspark_ip='localhost',
-                 simspark_port=3100,
-                 teamname='DAInamite',
-                 player_id=0,
-                 sync_mode=True):
-        super(ForwardKinematicsAgent, self).__init__(simspark_ip, simspark_port, teamname, player_id, sync_mode)
-        self.transforms = {n: identity(4) for n in self.joint_names}
+	def __init__(self, simspark_ip='localhost',
+		simspark_port=3100,
+		teamname='DAInamite',
+		player_id=0,
+		sync_mode=True):
+		super(ForwardKinematicsAgent, self).__init__(simspark_ip, simspark_port, teamname, player_id, sync_mode)
+		self.transforms = {n: identity(4) for n in self.joint_names}
 
-        # chains defines the name of chain and joints of the chain
-        self.chains = {'Head': ['HeadYaw', 'HeadPitch']
-                       # YOUR CODE HERE
-                       }
+		# chains defines the name of chain and joints of the chain
+		self.chains = {'Head': ['HeadYaw', 'HeadPitch'],
+				'LArm' : ['LShoulderPitch','LShoulderRoll','LElbowYaw','LElbowRoll'],
+				'LLeg' : ['LHipYawPitch','LHipRoll','LHipPitch','LKneePitch','LAnklePitch','LAnkleRoll'],
+				'RLeg' : ['RHipYawPitch','RHipRoll','RHipPitch','RKneePitch','RAnklePitch','RAnkleRoll'],
+				'RArm' : ['RShoulderPitch','RShoulderRoll','RElbowYaw','RElbowRoll']
+				}
 
-    def think(self, perception):
-        self.forward_kinematics(perception.joint)
-        return super(ForwardKinematicsAgent, self).think(perception)
+	def think(self, perception):
+		self.forward_kinematics(perception.joint)
+		return super(ForwardKinematicsAgent, self).think(perception)
 
-    def local_trans(self, joint_name, joint_angle):
-        '''calculate local transformation of one joint
+	def local_trans(self, joint_name, joint_angle):
+		'''calculate local transformation of one joint
 
-        :param str joint_name: the name of joint
-        :param float joint_angle: the angle of joint in radians
-        :return: transformation
-        :rtype: 4x4 matrix
-        '''
-        T = matrix()
-        # YOUR CODE HERE
+		:param str joint_name: the name of joint
+		:param float joint_angle: the angle of joint in radians
+		:return: transformation
+		:rtype: 4x4 matrix
+		'''
+		T = matrix(0)
+		s = math.sin(joint_angle)
+		c = math.cos(joint_angle)
+		'''print T, s, c'''
+		print joint_name
+		if 'Pitch' in joint_name :
+			T = T +([[c,0,s,0],
+				 [0,1,0,0],
+				[-s,0,c,0],
+				[0,0,0,1]])
+			if "Shoulder" in joint_name :
+				T = T + ([[0,0,0,0],
+		  			[0,0,0,98],
+					[0,0,0,100],
+					[0,0,0,0]])
 
-        return T
+			if "Knee" in joint_name :
+				T = T + ([[0,0,0,0],
+		  			[0,0,0,0],
+					[0,0,0,-102.9],
+					[0,0,0,0]])
+			if "Ankle" in joint_name :
+				T = T + ([[0,0,0,0],
+					[0,0,0,50],
+					[0,0,0,-85],
+					[0,0,0,0]])
+		elif 'Yaw' in joint_name :
+			T = T + ([[c,s,0,0],
+				[-s,c,0,0],
+				[0,0,0,0],
+				[0,0,0,1]])
+			if 'Head' in joint_name :
+				T = T + ([[0,0,0,0],
+					[0,0,0,0],
+					[0,0,0,126.5],
+					[0,0,0,0]])
+			if "Elbow" in joint_name :
+				T = T + ([[0,0,0,105],
+					[0,0,0,15],
+					[0,0,0,0],
+					[0,0,0,0]])
+			if "Wrist" in joint_name :
+				T = T + ([[0,0,0,55.95],
+					[0,0,0,0],
+					[0,0,0,0],
+					[0,0,0,0]])
+			if "Hip" in joint_name :
+				T = T + ([[0,0,0,0],
+					[0,0,0,50],
+					[0,0,0,-85],
+					[0,0,0,0]])
+		elif 'Roll' in joint_name :
+				T = T + ([[1,0,0,0],
+					[0,c,-s,0],
+					[0,s,c,0],
+					[0,0,0,1]])
 
-    def forward_kinematics(self, joints):
-        '''forward kinematics
+		return T
 
-        :param joints: {joint_name: joint_angle}
-        '''
-        for chain_joints in self.chains.values():
-            T = identity(4)
-            for joint in chain_joints:
-                angle = joints[joint]
-                Tl = local_trans(joint, angle)
-                # YOUR CODE HERE
+	def forward_kinematics(self, joints):
+		for chain_joints in self.chains.values():
+			T = identity(4)
 
-                self.transforms[joint] = T
+			for joint in chain_joints:
+				angle = joints[joint]
+				Tl = self.local_trans(joint, angle)
+
+				# YOUR CODE HERE
+				T = T * Tl
+				self.transforms[joint] = T
+
 
 if __name__ == '__main__':
     agent = ForwardKinematicsAgent()
--- kinematics/inverse_kinematics.py
+++ kinematics/inverse_kinematics.py
@@ -1,12 +1,11 @@
-'''In this exercise you need to implemente inverse kinematics for NAO's legs
-
+'''In this exercise you need to implement inverse kinematics for NAO's legs
 * Tasks:
-    1. solve inverse kinemtatics for NAO's legs by using analytical or numerical method.
+    1. solve inverse kinematics for NAO's legs by using analytical or numerical method.
        You may need documentation of NAO's leg:
        http://doc.aldebaran.com/2-1/family/nao_h21/joints_h21.html
        http://doc.aldebaran.com/2-1/family/nao_h21/links_h21.html
-    2. use the results of inverse kinemtatics to control NAO's legs (in InverseKinematicsAgent.set_transforms)
-       and test your inverse kinemtatics implementation.
+    2. use the results of inverse kinematics to control NAO's legs (in InverseKinematicsAgent.set_transforms)
+       and test your inverse kinematics implementation.
 '''
 
 
@@ -17,20 +16,67 @@ from numpy.matlib import identity
 class InverseKinematicsAgent(ForwardKinematicsAgent):
     def inverse_kinematics(self, effector_name, transform):
         '''solve the inverse kinematics
-
         :param str effector_name: name of end effector, e.g. LLeg, RLeg
         :param transform: 4x4 transform matrix
         :return: list of joint angles
         '''
-        joint_angles = []
-        # YOUR CODE HERE
+        joint_angles = {}
+        for chain in self.chains[effector_name]:
+            joint_angles[chain] = joints[chain]
+
+        error = 0
+        error_limit = 1e-4
+        lambda_ = 0.001
+        theta=0
+        target = self.from_trans(transform)
+        chain_of_effector = self.chains[effector_name]
+
+        while True:
+            self.forward_kinematics(joint_angles)
+            T = []
+            for chain in self.chains:
+                T.append(self.transforms[chain])
+
+            Te = np.matrix([self.from_trans(T[-1])])
+            e = target - Te
+            T = np.matrix([self.from_trans(i) for i in T[0:-1]])
+            J = Te - T
+            J = J.T
+            J[-1, :] = 1  # angular velocity
+            JJT = np.dot(J, J.T)
+            d_theta = lambda_ * J.T * JJT.I * e.T
+            theta += np.asarray(d_theta.T)[0]
+
+            i = 0
+            for chain in self.chains:
+                joint_angles[chain] += np.asarray(d_theta.T)[0][i]
+                i +=1
+            error = np.linalg.norm(d_theta)
+            if error < error_limit:
+                return joint_angles
         return joint_angles
 
     def set_transforms(self, effector_name, transform):
         '''solve the inverse kinematics and control joints use the results
         '''
         # YOUR CODE HERE
-        self.keyframes = ([], [], [])  # the result joint angles have to fill in
+        joint_angles = self.inverse_kinematics(effector_name, transform)
+        chain = self.chains[effector_name]
+        self.keyframes = (chain, [[0, 1]] * len(chain), joint_angles)  # the result joint angles have to fill in
+        return joint_angles
+        
+    def from_trans(self, m):
+        theta_x, theta_y, theta_z = 0,0,0
+        #x
+        if m[0, 0] == 1:
+            theta_x = np.arctan2(m[2, 1], m[1, 1])
+        #y
+        elif m[1, 1] == 1:
+            theta_y = np.arctan2(m[0, 2], m[0, 0])
+        #z
+        elif m[2, 2] == 1:
+            theta_z = np.arctan2(m[1, 0], m[0, 0])
+        return np.array([m[3, 0], m[3, 1], m[3, 2], theta_x, theta_y, theta_z])
 
 if __name__ == '__main__':
     agent = InverseKinematicsAgent()
@@ -39,4 +85,4 @@ if __name__ == '__main__':
     T[-1, 1] = 0.05
     T[-1, 2] = 0.26
     agent.set_transforms('LLeg', T)
-    agent.run()
+agent.run()
