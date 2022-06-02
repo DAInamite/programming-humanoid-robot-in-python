c087a57850e94161b72d1b33e7cedf9e0172e1e2
Yuan <yuan.xu@dai-labor.de>
2022-06-02 14:26:50 +0200

    use releative import for python3

--- joint_control/keyframes/__init__.py
+++ joint_control/keyframes/__init__.py
@@ -1,10 +1,10 @@
 '''Keyframes from Aldebaran
 '''
 
-from leftBackToStand import leftBackToStand
-from leftBellyToStand import leftBellyToStand
-from rightBackToStand import rightBackToStand
-from rightBellyToStand import rightBellyToStand
+from .leftBackToStand import leftBackToStand
+from .leftBellyToStand import leftBellyToStand
+from .rightBackToStand import rightBackToStand
+from .rightBellyToStand import rightBellyToStand
 
-from hello import hello
-from wipe_forehead import wipe_forehead
+from .hello import hello
+from .wipe_forehead import wipe_forehead
