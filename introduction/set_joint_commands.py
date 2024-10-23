13610e093f7e017bc29cc240cb7e493d3728aba0
Yamar100 <88257668+Yamar100@users.noreply.github.com>
2024-10-23 13:07:57 +0200

    Update set_joint_commands.py

--- introduction/set_joint_commands.py
+++ introduction/set_joint_commands.py
@@ -21,7 +21,10 @@ class MyAgent(SparkAgent):
     def think(self, perception):
         action = super(MyAgent, self).think(perception)
         # YOUR CODE HERE
-
+        
+        action.stiffness['lae1'] = 0
+        action.speed.['he1'] = 0.1
+        
         return action
 
 if '__main__' == __name__:
